873baa0cf2f9b15986828f7a9b54f6bdc86cff06
Dannybuffet <dannyreliford@icloud.com>
2023-05-19 13:48:10 -0400

    Update analyzer.py

--- earthquake_analyzer/analyzer.py
+++ earthquake_analyzer/analyzer.py
@@ -4,67 +4,64 @@ from collections import defaultdict
 
 
 class EarthquakeDataReader:
-    # Reads earthquake data from CSV file.
-
     @staticmethod
     def read_csv_file(filename):
-        # Reads CSV file and yields each row.
+        """
+        Reads a CSV file and yields rows, skipping the header row.
+        """
         try:
             with open(filename, 'r') as file:
                 csv_reader = csv.reader(file)
-                next(csv_reader) 
+                next(csv_reader)  # Skip header row
                 for row in csv_reader:
                     yield row
         except FileNotFoundError as e:
             raise FileNotFoundError(f"File '{filename}' not found.") from e
         except Exception as e:
             raise Exception(f"Error reading CSV file: {str(e)}") from e
-        
-
-class EarthquakeDataProcessor:
-    # Handles earthquake data processing.
-
-    def __init__(self, analyzer):
-        self.analyzer = analyzer
-
-    def process_data(self, data_rows):
-        # Processes the data rows.
-        try:
-            for row in data_rows:
-                self.analyzer.calculate_average_magnitudes(row)
-                self.analyzer.calculate_earthquakes_per_day(row)
-                self.analyzer.calculate_earthquakes_per_location(row)
-        except Exception as e:
-            raise Exception(f"Error processing data: {str(e)}") from e
 
 
 class EarthquakeDataAnalyzer:
-    # Analyzes earthquake data.
-
     def __init__(self, location_index, date_index, magnitude_index):
+        """
+        Initializes the EarthquakeDataAnalyzer.
+        """
         self.location_index = location_index
         self.date_index = date_index
         self.magnitude_index = magnitude_index
-        self.location_avg_magnitudes = defaultdict(lambda: {'count': 0, 'mean': 0})
+        self.location_avg_magnitudes = defaultdict(lambda: {'mean': 0, 'count': 0})
         self.location_earthquake_count = defaultdict(int)
         self.earthquake_count_per_day = defaultdict(int)
 
+    def process_data(self, data_rows):
+        """
+        Processes the data rows, calculating average magnitudes, earthquakes per day, and earthquakes per location.
+        """
+        for row in data_rows:
+            self.calculate_average_magnitudes(row)
+            self.calculate_earthquakes_per_day(row)
+            self.calculate_earthquakes_per_location(row)
+
     def calculate_average_magnitudes(self, row):
-        # Calculates the average earthquake magnitude for each location.
+        """
+        Calculates the average magnitudes for each location.
+        """
         try:
             magnitude = float(row[self.magnitude_index])
             location_source = row[self.location_index]
 
             data = self.location_avg_magnitudes[location_source]
             data['count'] += 1
-            delta = magnitude - data['mean']
-            data['mean'] += delta / data['count']
+            total_magnitude = data['mean'] * (data['count'] - 1)
+            data['mean'] = (total_magnitude + magnitude) / data['count']
 
         except (ValueError, IndexError) as e:
             raise Exception(f"Error computing average magnitudes: {str(e)}") from e
 
     def calculate_earthquakes_per_location(self, row):
-        # Calculates the number of earthquakes per location.
+        """
+        Calculates the number of earthquakes per location.
+        """
         try:
             location = row[self.location_index]
             self.location_earthquake_count[location] += 1
@@ -72,7 +69,9 @@ class EarthquakeDataAnalyzer:
             raise Exception(f"Error calculating earthquakes per location: {str(e)}") from e
 
     def calculate_earthquakes_per_day(self, row):
-        # Calculates the number of earthquakes per day.
+        """
+        Calculates the number of earthquakes per day.
+        """
         try:
             date_str = row[self.date_index]
             date = self._parse_date(date_str)
@@ -80,30 +79,38 @@ class EarthquakeDataAnalyzer:
         except Exception as e:
             raise Exception(f"Error calculating earthquakes per day: {str(e)}") from e
 
-    def analyze_earthquake_data(self):
-        # Analyzes earthquake data and return the location with the most earthquakes.
+    def calculate_location_with_most_earthquakes(self):
+        """
+        Determines the location with the most earthquakes.
+        """
         try:
-            location_with_most_earthquakes = max(self.location_earthquake_count, key=self.location_earthquake_count.get)
+            location_with_most_earthquakes = max(
+                self.location_earthquake_count, key=self.location_earthquake_count.get
+            )
             return location_with_most_earthquakes
         except ValueError as e:
             raise ValueError("No earthquake data available.") from e
         except Exception as e:
             raise Exception(f"Error analyzing earthquake data: {str(e)}") from e
 
-    def analyze_earthquakes_per_day(self, timezone):
-        # Analyzes the number of earthquakes per day by timezone.
+    def calculate_earthquakes_per_day_by_timezone(self, timezone):
+        """
+        Analyzes the number of earthquakes per day by timezone.
+        """
         try:
             timezone_earthquake_count = defaultdict(int)
             for date, count in self.earthquake_count_per_day.items():
-                date_with_timezone = date.astimezone(timezone).date() 
+                date_with_timezone = date.astimezone(timezone).date()
                 timezone_earthquake_count[date_with_timezone] += count
             return timezone_earthquake_count
         except Exception as e:
             raise Exception(f"Error analyzing earthquakes per day: {str(e)}") from e
-        
+
     @staticmethod
     def _parse_date(date_str):
-        # Parses the date string into a datetime object.
+        """
+        Parses the date string and returns a datetime object.
+        """
         date = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%fZ")
         return date
 
