9aad2c7518f750011d1c69c861e1a1b206dc8b96
Dannybuffet <dannyreliford@icloud.com>
2023-05-19 12:44:45 -0400

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from earthquake_analyzer.analyzer import EarthquakeDataAnalyzer, EarthquakeDataReader, EarthquakeDataHandler
+from earthquake_analyzer.analyzer import EarthquakeDataAnalyzer, EarthquakeDataReader, EarthquakeDataProcessor
 
 class EarthquakeDataReaderTests(unittest.TestCase):
     def test_read_csv_file(self):
@@ -16,49 +16,51 @@ class EarthquakeDataReaderTests(unittest.TestCase):
         data = list(data_reader.read_csv_file('data/test_data.csv'))
 
         expected_data = [
-            (0, ['1', 'California', '5.6']),
-            (1, ['2', 'Japan', '7.2'])
+            ['1', 'California', '5.6'],
+            ['2', 'Japan', '7.2']
         ]
 
         self.assertEqual(data, expected_data)
 
 
-class EarthquakeDataHandlerTests(unittest.TestCase):
-    # Test to verify the `process_data` method in the EarthquakeDataHandler.
+class EarthquakeDataProcessorTests(unittest.TestCase):
+    # Test to verify the process_data method in the EarthquakeDataProcessor.
     def test_process_data(self):
         analyzer = MagicMock()
-        handler = EarthquakeDataHandler(analyzer, location_index=1, magnitude_index=2)
+        processor = EarthquakeDataProcessor(analyzer)
 
-        row = (0, ['1', 'California', '5.6'])
-        handler.process_data(row)
+        row = ['1', 'California', '5.6']
+        processor.process_data([row])
 
-        analyzer.calculate_average_magnitudes.assert_called_once_with(['1', 'California', '5.6'])
+        analyzer.calculate_average_magnitudes.assert_called_once_with(row)
+        analyzer.calculate_earthquakes_per_location.assert_called_once_with(row)
+        analyzer.calculate_earthquakes_per_day.assert_called_once_with(row)
 
 
 class EarthquakeDataAnalyzerTests(unittest.TestCase):
     def setUp(self):
         # Test setup method for the EarthquakeDataAnalyzer
-        self.data_reader = MagicMock()
-        self.analyzer = EarthquakeDataAnalyzer(
-            self.data_reader, location_index=1, date_index=2, magnitude_index=3
-        )
+        self.analyzer = EarthquakeDataAnalyzer(location_index=1, date_index=2, magnitude_index=3)
 
 
     def test_calculate_earthquakes_per_location(self):
-        #Test to verify the functionality of the calculate_earthquakes_per_location method
-        self.data_reader.read_csv_file.return_value = [
-            (0, ['1', 'California', '2023-05-18', '5.6']),
-            (1, ['2', 'Japan', '2023-05-18', '7.2']),
-            (2, ['3', 'California', '2023-05-18', '6.4']),
+        # Test to verify the functionality of the calculate_earthquakes_per_location method
+        self.analyzer.location_earthquake_count = defaultdict(int)
+
+        rows = [
+            ['1', 'California', '2023-05-18', '5.6'],
+            ['2', 'Japan', '2023-05-18', '7.2'],
+            ['3', 'California', '2023-05-18', '6.4'],
         ]
 
-        earthquake_count = self.analyzer.calculate_earthquakes_per_location('data.csv')
+        for row in rows:
+            self.analyzer.calculate_earthquakes_per_location(row)
 
         expected_count = defaultdict(int)
         expected_count['California'] = 2
         expected_count['Japan'] = 1
 
-        self.assertEqual(earthquake_count, expected_count)
+        self.assertEqual(self.analyzer.location_earthquake_count, expected_count)
 
 
     def test_calculate_average_magnitudes(self):
@@ -74,3 +76,4 @@ class EarthquakeDataAnalyzerTests(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
